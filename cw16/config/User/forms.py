7ef0246f56d53e22c7c8d206f8092a883d16b0a0
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-03 05:47:21 -0700

    chore: confirm password func added

--- cw16/config/User/forms.py
+++ cw16/config/User/forms.py
@@ -1,9 +1,17 @@
-from django.forms import ModelForm, CharField, PasswordInput
+from django.forms import ModelForm, CharField, PasswordInput, ValidationError
 from .models import CustomUser
 
 
 class UserForm(ModelForm):
-    passwordconfirm=CharField(widget=PasswordInput)
+    confirm_password=CharField(widget=PasswordInput)
     class Meta:
         model=CustomUser
         fields=('username', 'email', 'password')
+
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+        if password != confirm_password:
+            raise ValidationError("password and confirm_password does not match")
+        return cleaned_data
