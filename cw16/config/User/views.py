9ca3929f365769f4687eb49e44b201d7c6221b0c
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-04 01:10:32 -0700

    chore:edit

--- cw16/config/User/views.py
+++ cw16/config/User/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from .models import Users
+from .models import CustomUser
 from django.shortcuts import get_list_or_404
 
 def user_tasks(request):
-    all_users=get_list_or_404(Users)
+    all_users=get_list_or_404(CustomUser)
     return render(request, 'User/all_users.html', {'all_users': all_users})
+
+
+def user_login(request):
+    if request.method=="POST":
+        cd=request.POST(CustomUser)
+    
