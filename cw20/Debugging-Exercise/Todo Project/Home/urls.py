7b117f0a690f49d46fa353032fc91bcdb27c4370
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-08 09:45:16 -0700

    fixed: replace pk

--- cw20/Debugging-Exercise/Todo Project/Home/urls.py
+++ cw20/Debugging-Exercise/Todo Project/Home/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('todos/', views.TodoListView.as_view(), name='todo_list'),
-    path('todos/<int:pk>/', views.TodoDetailView.as_view(), name='todo_detail'),
+    path('todos/<int:id>/', views.TodoDetailView.as_view(), name='todo_detail'),
 ]
