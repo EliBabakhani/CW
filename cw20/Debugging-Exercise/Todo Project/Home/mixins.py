a6919950d4b61a3b5751762da1e458e57d932d2c
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-10 22:50:55 -0700

    feat: todoowner added

--- cw20/Debugging-Exercise/Todo Project/Home/mixins.py
+++ cw20/Debugging-Exercise/Todo Project/Home/mixins.py
@@ -27,12 +27,36 @@ class TodoMixin:
             todo.save()
             return redirect('thank_you')
         return render(request, self.template_name, {'todo': todo})
+    
+
+class TodoOwnerRequiredMixin:
+    form_class = UpdateForm
+    template_name = None
+
+    def dispatch(self, request, *args, **kwargs):
+        todo = Todo.objects.get(id=kwargs['id'])
+        if not todo.user == request.user:
+            raise PermissionDenied
+        return super(TodoOwnerRequiredMixin, self).dispatch(request, *args, **kwargs)
+
+
+    def get(self, request, id):
+        todo = Todo.objects.get(id=id)
+        return render(request, self.template_name, {'todo': todo})
+
+    def post(self, request, id):
+        todo = Todo.objects.get(id=id)
+        form = self.form_class(request.POST, instance=todo)
+        if form.is_valid():
+            todo.save()
+            return redirect('thank_you')
+        return render(request, self.template_name, {'todo': todo})
 
 
 class ProfileMixin:
     form_class=ProfileForm
     template_name=None
-
+    
     def dispatch(self, request, *args, **kwargs):
         profile = User.objects.get(id=kwargs['id'])
         if not profile.is_authenticated():
