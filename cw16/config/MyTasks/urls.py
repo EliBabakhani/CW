496060eb219f2b1daf90655912957e529e746216
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-17 06:59:20 -0700

    chore: edit update url based on generic

--- cw16/config/MyTasks/urls.py
+++ cw16/config/MyTasks/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('result', views.search_result, name='result'),
     path('usertasks/<slug:user>', views.see_user_tasks, name='usertasks'),
     path('profile/<int:id>', views.ProfileView.as_view(), name='profile'),
-    path('update/<int:id>', views.TodoUpdateView.as_view(), name='update'),
+    path('update/<int:id>', views.TaskUpdateView.as_view(), name='update'),
 ]
