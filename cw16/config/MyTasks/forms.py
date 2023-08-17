677ca2e34ef15c48ca34d064c62af10d0a23b980
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-17 05:04:38 -0700

    chore: task for replace with modelfrom

--- cw16/config/MyTasks/forms.py
+++ cw16/config/MyTasks/forms.py
@@ -1,12 +1,11 @@
 from django import forms
 from .models import Task
 
-class TaskForm(forms.Form):
-    title=forms.CharField(label='Title')
-    description=forms.CharField(label='Description')
-    due_date=forms.DateTimeField(label='Deadline')
-    tags=forms.CharField(label='Tags')
-    status_field=forms.CharField(label='Status')
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'due_date', 'tags', 'status_field']
     
 
 class MyTaskForm(forms.ModelForm):
