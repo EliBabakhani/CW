43f747a9a636fcb0dbd0531c2c004554cccf88fe
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-18 00:10:20 -0700

    chore: cw 21 part 1 sort numberof task

--- cw16/config/User/admin.py
+++ cw16/config/User/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import *
+from MyTasks.models import Task
 
-admin.site.register(CustomUser)
+
+
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display=['username','is_active','number_of_task']
+
+    @admin.display(ordering='username')
+    def number_of_task(self,user):
+        query=Task.objects.filter(user=user).count()
+        print(query)
+        return query
+        
+
+    
