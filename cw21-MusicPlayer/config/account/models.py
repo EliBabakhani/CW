5ed02cfa8009bfdc02b5d87efc242c98f05aa7e4
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-31 14:35:49 -0700

    feat: account models created

--- cw21-MusicPlayer/config/account/models.py
+++ cw21-MusicPlayer/config/account/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from music.models import Genre
 
-# Create your models here.
+class Artist(models.Model):
+    name=models.CharField(max_length=100)
+    bio=models.TextField()
+    image=models.ImageField(upload_to='/media')
+
+
+class User(AbstractUser):
+    ACCOUNT_CHOICES = (
+        ('Normal', 'NORMAL'),
+        ('VIP', 'VIP'),
+    )
+    account_type = models.CharField(max_length=1, choices=ACCOUNT_CHOICES)
+    image=models.ImageField(upload_to='/media')
+    interests=models.ManyToManyField(Genre)
+
