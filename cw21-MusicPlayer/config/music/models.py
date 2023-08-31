640e7ce32cbbdb906966fd34c91143bcb25cd1a8
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-31 13:53:04 -0700

    feat: music models created

--- cw21-MusicPlayer/config/music/models.py
+++ cw21-MusicPlayer/config/music/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from account.models import Artist,User
 
-# Create your models here.
+class Genre(models.Model):
+    name=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Song(models.Model):
+    title=models.CharField(max_length=100)
+    artist=models.ManyToManyField(Artist, on_delete=models.CASCADE)
+    upload_at=models.DateTimeField(auto_now=True)
+    cover_photo=models.ImageField()
+    audio_file=models.FileField(upload_to="uploads/%Y/%m/")
+    genre=models.ForeignKey(Genre,on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
+    
+
+class PlayList(models.Model):
+    title=models.CharField(max_length=100)
+    description=models.TextField()
+    owner=models.ForeignKey(User,on_delete=models.CASCADE)
+    song=models.ManyToManyField(Song, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
