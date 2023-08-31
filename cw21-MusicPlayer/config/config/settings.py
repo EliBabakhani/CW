5a32514dcd9a6cbed670d02ff565f63a37c23f37
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-31 13:30:20 -0700

    feat: app installed in setting

--- cw21-MusicPlayer/config/config/settings.py
+++ cw21-MusicPlayer/config/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account.apps.AccountConfig',
+    'comment.apps.CommentConfig',
+    'music.apps.MusicConfig',
 ]
 
 MIDDLEWARE = [
