42701aee8497ceadf2196074233d9fd341f992f9
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-31 15:04:16 -0700

    chore: account model edited to debug error

--- cw21-MusicPlayer/config/account/models.py
+++ cw21-MusicPlayer/config/account/models.py
@@ -1,19 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from music.models import Genre
+from django.contrib.auth.models import AbstractUser, Group, Permission
+
 
 class Artist(models.Model):
     name=models.CharField(max_length=100)
     bio=models.TextField()
-    image=models.ImageField(upload_to='/media')
+    image=models.ImageField(upload_to='media')
+
+    def __str__(self) -> str:
+        return self.name
 
 
-class User(AbstractUser):
+class Users(AbstractUser):
     ACCOUNT_CHOICES = (
         ('Normal', 'NORMAL'),
         ('VIP', 'VIP'),
     )
-    account_type = models.CharField(max_length=1, choices=ACCOUNT_CHOICES)
-    image=models.ImageField(upload_to='/media')
-    interests=models.ManyToManyField(Genre)
+    account_type = models.CharField(max_length=6, choices=ACCOUNT_CHOICES)
+    image=models.ImageField(upload_to='media')
+# WHY???
+    groups = models.ManyToManyField(Group, verbose_name='groups', blank=True, related_name='user_accounts')
+    user_permissions = models.ManyToManyField(
+        Permission, verbose_name='user permissions', blank=True, related_name='user_accounts_permissions'
+    )
+
+    def __str__(self) -> str:
+        return self.username
 
