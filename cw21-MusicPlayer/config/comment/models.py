f0574aa7b9d558b91600220f268ff516b1146eb1
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-08-31 13:30:36 -0700

    feat: comment moels created

--- cw21-MusicPlayer/config/comment/models.py
+++ cw21-MusicPlayer/config/comment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from music.models import Song
+from account.models import User
 
-# Create your models here.
+class Like(models.Model):
+    song=models.ForeignKey(Song, on_delete=models.CASCADE)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    body=models.TextField()
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    song=models.ForeignKey(Song, on_delete=models.CASCADE)
