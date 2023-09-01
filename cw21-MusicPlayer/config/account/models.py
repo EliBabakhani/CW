29e01a485d04f5901490689999da4630d1728743
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-01 00:33:55 -0700

    chore: user registered at setting

--- cw21-MusicPlayer/config/account/models.py
+++ cw21-MusicPlayer/config/account/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import AbstractUser, Group, Permission
 
 
 class Artist(models.Model):
@@ -12,18 +11,13 @@ class Artist(models.Model):
         return self.name
 
 
-class Users(AbstractUser):
+class User(AbstractUser):
     ACCOUNT_CHOICES = (
         ('Normal', 'NORMAL'),
         ('VIP', 'VIP'),
     )
     account_type = models.CharField(max_length=6, choices=ACCOUNT_CHOICES)
     image=models.ImageField(upload_to='media')
-# WHY???
-    groups = models.ManyToManyField(Group, verbose_name='groups', blank=True, related_name='user_accounts')
-    user_permissions = models.ManyToManyField(
-        Permission, verbose_name='user permissions', blank=True, related_name='user_accounts_permissions'
-    )
 
     def __str__(self) -> str:
         return self.username
