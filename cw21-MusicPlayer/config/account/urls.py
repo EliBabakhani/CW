af7562512847d4694e89c67bfea670b8a1422d2f
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-01 17:55:03 -0700

    feat: path for login, logout and register added

--- cw21-MusicPlayer/config/account/urls.py
+++ cw21-MusicPlayer/config/account/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from account import views
 
+
+app_name='account'
 urlpatterns = [
-    path('login',views.index, name='ind')
+    path('login',views.UserLoginView.as_view(), name='user_login'),
+    path('register', views.UserRegisterView.as_view(), name='user_register'),
+    path('logout', views.UserLogoutView.as_view(), name='user_logout'),
 ]
