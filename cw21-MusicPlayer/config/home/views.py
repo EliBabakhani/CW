8c9549b68b8f78ffd831485589d470418499424e
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-01 18:00:04 -0700

    feat: all songs view in home

--- cw21-MusicPlayer/config/home/views.py
+++ cw21-MusicPlayer/config/home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from music.models import Song
 
 def index(request):
-    return render(request,'base.html')
+    songs=Song.objects.all()
+    return render(request,'home/index.html', context={'songs': songs})
