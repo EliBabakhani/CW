98c22362313928f21c5054496ef921d27983de48
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-01 17:57:15 -0700

    debug: remove migrations and db for model chages

--- cw21-MusicPlayer/config/music/views.py
+++ cw21-MusicPlayer/config/music/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+from .models import Song
 
-# Create your views here.
+class SongListView(ListView):
+    model=Song
+    template_name='music/all_songs.html'
+
+class SongDetailView(DetailView):
+    model=Song
+    template_name='music/song.html'
+    pk_url_kwarg = 'pk'
