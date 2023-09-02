b2a83226156d234d834296cbb1e3a09b32e46b3d
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-01 18:02:43 -0700

    feat: app name and pathes for all song and detail

--- cw21-MusicPlayer/config/music/urls.py
+++ cw21-MusicPlayer/config/music/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from music import views
 
+
+app_name='music'
 urlpatterns = [
+    path('songs', views.SongListView.as_view(), name='all_songs'),
+    path('song/<int:pk>', views.SongDetailView.as_view(), name='song'),
     
 ]
