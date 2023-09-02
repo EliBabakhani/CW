ab3ce5cc66fd2b731721736cf54b6b6399c1cde7
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-01 17:59:17 -0700

    chore: replace user to users

--- cw21-MusicPlayer/config/comment/models.py
+++ cw21-MusicPlayer/config/comment/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from music.models import Song
-from account.models import Users
+from account.models import User
 
 class Like(models.Model):
     song=models.ForeignKey(Song, on_delete=models.CASCADE)
-    user=models.ForeignKey(Users, on_delete=models.CASCADE)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Comment(models.Model):
     body=models.TextField()
-    user=models.ForeignKey(Users, on_delete=models.CASCADE)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
     song=models.ForeignKey(Song, on_delete=models.CASCADE)
