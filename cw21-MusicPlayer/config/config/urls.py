83b095b391763c499e148cc8d4e3dea70650d208
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-05 08:22:21 -0700

    chore: static and media patterns adedd to url

--- cw21-MusicPlayer/config/config/urls.py
+++ cw21-MusicPlayer/config/config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import settings
+from django.conf.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,6 @@ urlpatterns = [
     path('comment/', include('comment.urls')),
     path('music/',include('music.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
