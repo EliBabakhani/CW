7aceb2c9389877f2f6087aa6a182c319f96a7322
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-05 08:23:10 -0700

    feat: url path fpr create playlist added

--- cw21-MusicPlayer/config/music/urls.py
+++ cw21-MusicPlayer/config/music/urls.py
@@ -6,5 +6,7 @@ app_name='music'
 urlpatterns = [
     path('songs', views.SongListView.as_view(), name='all_songs'),
     path('song/<int:pk>', views.SongDetailView.as_view(), name='song'),
+    path('create_playlist', views.CreatePlylistView.as_view(),name='create_playlist'),
+    path('add_to_play_list/', views.AddMusicView.as_view(), name='add_to_playlist'),
     
 ]
