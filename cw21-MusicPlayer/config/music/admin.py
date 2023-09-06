42f9043928f1cbf08ed79ef13e46375f881af3fd
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-06 15:47:27 -0700

    feat: admin panel music added

--- cw21-MusicPlayer/config/music/admin.py
+++ cw21-MusicPlayer/config/music/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import Song, PlayList, Genre
 
-admin.site.register(Song)
-admin.site.register(PlayList)
 admin.site.register(Genre)
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display=['title','upload_at']
+    search_fields=['artist','title']
+    list_filter=['upload_at','artist']
+
+
+
+@admin.register(PlayList)
+class PlylistAdmin(admin.ModelAdmin):
+    list_display=['title', 'owner']
+    search_fields=['title','owner']
+    list_filter=['owner']
+
 
