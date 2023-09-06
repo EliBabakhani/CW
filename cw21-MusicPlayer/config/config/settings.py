1e5453cbe68b87bd76a5ecfff6d5b67b1321b0fb
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-06 15:46:46 -0700

    feat: email setup added

--- cw21-MusicPlayer/config/config/settings.py
+++ cw21-MusicPlayer/config/config/settings.py
@@ -135,3 +135,10 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+# Google 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'elnaz.babakhani71@gmail.com'
+EMAIL_HOST_PASSWORD = 'ickmpaxhzbowwdmv'
