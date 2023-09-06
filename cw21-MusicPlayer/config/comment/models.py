77d1279eadd1002c97f7d3c8de04a0d7088c0c04
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-06 15:42:46 -0700

    chore: comment model updated

--- cw21-MusicPlayer/config/comment/models.py
+++ cw21-MusicPlayer/config/comment/models.py
@@ -3,10 +3,15 @@ from music.models import Song
 from account.models import User
 
 class Like(models.Model):
-    song=models.ForeignKey(Song, on_delete=models.CASCADE)
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    song=models.ForeignKey(Song, on_delete=models.CASCADE, related_name='slike')
+    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name='ulike')
 
 class Comment(models.Model):
-    body=models.TextField()
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
-    song=models.ForeignKey(Song, on_delete=models.CASCADE)
+    body=models.TextField(null=True,blank=True)
+    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name='ucommnet')
+    song=models.ForeignKey(Song, on_delete=models.CASCADE, related_name='scommnet')
+    created=models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self) -> str:
+        return f'{self.user}-{self.body[:30]}'
