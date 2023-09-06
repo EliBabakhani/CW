69b33d057ef0ac1c599c46f84dea838164655edb
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-06 15:42:20 -0700

    chore: commnet create form added

--- cw21-MusicPlayer/config/comment/forms.py
+++ cw21-MusicPlayer/config/comment/forms.py
@@ -5,4 +5,5 @@ from .models import Comment
 class CreateCommentForm(forms.ModelForm):
     class Meta:
         model=Comment
-        fields=('body',)
+        fields=('body','song')
+        widgets={'body':forms.Textarea(attrs={'class':'form-control'}), 'song': forms.TextInput(attrs={'type': 'hidden'})}
