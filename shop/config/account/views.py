cd082e05e412d0332bd86cdd0b933af50b117d87
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-15 06:32:31 -0700

    chore: serializer name updated

--- shop/config/account/views.py
+++ shop/config/account/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from API.serialize_user import SerializerUser
+from API.serialize_user import SerializerRegisterUser
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 class RegisterView(APIView):
 
     def post(self, request):
-        serializer=SerializerUser(data=request.data)
+        serializer=SerializerRegisterUser(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data)
