d703d463c71871dce70047fd79351ee9e2e8bdd6
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-15 06:24:05 -0700

    feat: function for hash password added-serializer

--- shop/config/API/serialize_user.py
+++ shop/config/API/serialize_user.py
@@ -6,4 +6,12 @@ class SerializerUser(serializers.Serializer):
     class Meta:
         model=User
         fields=['id','email','phone','username','first_name','last_name','address']
+        
+        def create(self, validated_data):
+            password=validated_data.pop('password', None)
+            instance=self.Meta.model(**validated_data)
+            if password is not None:
+                instance.set_password(password)
+            instance.save()
+            return instance
 
