95ca699643e127e5d7efd8b5b07b6d03a311eee5
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-15 06:23:25 -0700

    feat: register view for user

--- shop/config/account/views.py
+++ shop/config/account/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from API.serialize_user import SerializerUser
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class RegisterView(APIView):
+
+    def post(self, request):
+        serializer=SerializerUser(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
