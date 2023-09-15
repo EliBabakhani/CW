a56ae40754d7d576d6f2b88adeb659af2127c04c
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-15 05:42:36 -0700

    feat: order model created

--- shop/config/order/models.py
+++ shop/config/order/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+class Order( models.Model):
+    cutomer=models.ForeignKey(User, on_delete=models.PROTECT)
+    discount=models.ForeignKey('Discount', on_delete=models.CASCADE)
+    payment=models.ForeignKey('Payment', on_delete=models.PROTECT)
 
-# Create your models here.
+    def __str__(self) -> str:
+        return self.cutomer
