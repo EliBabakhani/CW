f7a26396c3334ba8e27b2da3b0e15c6672e9d5f9
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-15 05:51:53 -0700

    feat: product models created

--- shop/config/product/models.py
+++ shop/config/product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.CharField(max_length=300)
+    price=models.DecimalField(decimal_places=2, max_digits=5)
+    quantity=models.IntegerField()
+    image=models.ImageField(upload_to='static')
+    category=models.ForeignKey('Category', on_delete=models.CASCADE)
+
+
+    def __str__(self) -> str:
+        return self.name
