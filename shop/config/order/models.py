addc262a38f7af224221214ad4cd71d0639f3359
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-15 05:43:27 -0700

    feat: discount and payment models created

--- shop/config/order/models.py
+++ shop/config/order/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from account.models import User
+from product.models import Product
+
 class Order( models.Model):
     cutomer=models.ForeignKey(User, on_delete=models.PROTECT)
     discount=models.ForeignKey('Discount', on_delete=models.CASCADE)
@@ -15,3 +18,12 @@ class OredrItem(models.Model):
 
     def __str__(self) -> str:
         return self.order
+
+class Discount(models.Model):
+    discount=models.DecimalField(decimal_places=2, max_digits=5)
+
+class Payment(models.Model):
+    is_paid=models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.is_paid
