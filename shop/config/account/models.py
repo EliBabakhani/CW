57e5098a20edc888e349ada15c90dd03f62e1a99
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-15 04:43:41 -0700

    feat: user model added

--- shop/config/account/models.py
+++ shop/config/account/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+
 class Address:
     address=models.CharField(max_length=200)
+
+
+class User(AbstractBaseUser):
+    email=models.EmailField()
+    phone=models.CharField(max_length=14)
+    username=models.CharField(max_length=50)
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=100)
+    password=models.CharField(max_length=50)
+    address=models.ForeignKey(Address, on_delete=models.CASCADE)
