8d25d43853c098312bd431c91b8db5594c2ed7fe
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-15 05:42:56 -0700

    feat: order item model created

--- shop/config/order/models.py
+++ shop/config/order/models.py
@@ -6,3 +6,12 @@ class Order( models.Model):
 
     def __str__(self) -> str:
         return self.cutomer
+
+class OredrItem(models.Model):
+    order=models.ForeignKey(Order,on_delete=models.CASCADE)
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity=models.CharField(max_length=10)
+    unit_price=models.DecimalField(decimal_places=2, max_digits=5)
+
+    def __str__(self) -> str:
+        return self.order
