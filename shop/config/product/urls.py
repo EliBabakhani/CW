27f4cb53a372e7a008c9b3976c3dfc2bda399314
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-21 05:40:03 -0700

    feat: add url for product search

--- shop/config/product/urls.py
+++ shop/config/product/urls.py
@@ -1,3 +1,8 @@
+from django.urls import path
+from . import views
+
+
+
 urlpatterns = [
     path('listproducts', views.ProductListView.as_view() ),
     path('search', views.SearchProductView.as_view()),
