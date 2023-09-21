539855da6a37a10800fdf44d570bae9f6cbb7cbb
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-21 02:16:50 -0700

    remove: id from fields

--- shop/config/API/serialize_user.py
+++ shop/config/API/serialize_user.py
@@ -5,7 +5,7 @@ from account.models import User
 class SerializerUser(serializers.Serializer):
     class Meta:
         model=User
-        fields=['id','email','phone','username','first_name','last_name','address']
+        fields=['email','phone','username','first_name','last_name','address']
         
         def create(self, validated_data):
             password=validated_data.pop('password', None)
