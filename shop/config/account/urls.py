b0e5d43cdcc9df91e401a6a39ad3d4b3a499a733
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-21 02:09:09 -0700

    feat: url for loginrequired view

--- shop/config/account/urls.py
+++ shop/config/account/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register', views.RegisterView.as_view(), name='user_register'),
     path('sendotp',views.SendOTPView.as_view()),
     path('verify', views.VerifyOTP.as_view()),
+    path('requiredlogin', views.LoginRequiredView.as_view()),
 ]
