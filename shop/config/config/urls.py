ed179950abee6e43d2790c5b0632d8f8fcd1309e
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-21 02:15:39 -0700

    remove: url of api app

--- shop/config/config/urls.py
+++ shop/config/config/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-    path('api/', include('api.urls')),
     path('product/', include('product.urls')),
     path('order/', include('order.urls')),
 ]
