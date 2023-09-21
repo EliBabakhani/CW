b1fdb69aabed8cf012d2169d0d40af12ec298482
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-21 02:11:24 -0700

    feat: view for verifying otp

--- shop/config/account/views.py
+++ shop/config/account/views.py
@@ -21,3 +21,14 @@ class SendOTPView(APIView):
             return Response (data={'message':"200"})
 
 
+class VerifyOTP(APIView):
+    
+    def post(self, request):
+        serliazer=LoginOTPSerializer(data=request.data, context={'request':request})
+        if serliazer.is_valid(raise_exception=True):
+            user=User.objects.get(phone=request.session.get('phone'))
+            access_token=user.get_access_token()
+            refresh_token=user.get_refresh_token()
+            return Response(data={'message':"success", "AT":access_token, "RT":refresh_token})
+
+
